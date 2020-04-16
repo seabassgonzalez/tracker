3f835d00e5464bdf97bb0838276f5c31e09d9a39
seabassgonzalez <seabassgonzalez@gmail.com>
2020-04-15 17:00:07 -0700

    Create dev script using nodemon; configure mongoose connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://seabassgonzalez:gunitsa5@clusterduck-pp2lr.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+	console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+	console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
 	res.send('Hi there!');
