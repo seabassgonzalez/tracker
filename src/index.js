8025edbff59280b2f7c40e844f17c7306193d75b
seabassgonzalez <seabassgonzalez@gmail.com>
2020-04-16 13:08:36 -0700

    Import body parser, authRoutes, use authroutes; import express, store express.Router() in router, router.post route for /signup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://seabassgonzalez:gunitsa5@clusterduck-pp2lr.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(mongoUri, {
