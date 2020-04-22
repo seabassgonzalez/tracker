7d42fa515c53737e96c19060a0f1dfa4fb1236ad
seabassgonzalez <seabassgonzalez@gmail.com>
2020-04-22 09:57:11 -0700

    Set for exporting User model

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
 	email: {
 		type: String,
-		unique: true
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
 	}
 });
+
+mongoose.model('User', userSchema);
