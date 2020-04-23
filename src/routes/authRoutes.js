47209d4741338a3a668150e1c1638df9c4becc67
seabassgonzalez <seabassgonzalez@gmail.com>
2020-04-23 12:19:35 -0700

    Condense same key and value

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -13,7 +13,7 @@ router.post('/signup', async (req, res) => {
 		await user.save();
 		
 		const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-		res.send({ token: token });
+		res.send({ token });
 	} catch (err) {
 		return res.status(422).send(err.message);
 	}
