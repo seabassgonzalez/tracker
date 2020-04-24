fb7b35455d8eb6deef4bff8af8371a4e1f6c944d
seabassgonzalez <seabassgonzalez@gmail.com>
2020-04-23 17:43:38 -0700

    Import requireAuth in index; set req.user and call next() in requireAuth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -18,5 +18,7 @@ module.exports = (req, res, next) => {
 		const { userId } = payload;
 
 		const user = await User.findById(userId);
+		req.user = user;
+		next();
 	});
 };	
